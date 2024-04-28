c81732c2983197f8f391bb4c62b5d4d68ecb718c
Julio Cesar <105808087+Julioctenorio@users.noreply.github.com>
2024-04-28 12:54:42 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -144,7 +144,7 @@ checkoutBtn.addEventListener('click', function(){
   const isOpen = checkIsOpen();
   if(!isOpen){
     Toastify({
-      text: "Ops, estamos fechados no momento!",
+      text: "Ops, as maquinas estão descansando neste momento!",
       duration: 3000,
       close: true,
       gravity: "top", // `top` or `bottom`
